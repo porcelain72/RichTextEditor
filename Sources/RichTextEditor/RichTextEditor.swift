80114ffbb26091579a70ab5a526f243236d009d6
Peter Macdonald <porcelain2015@gmail.com>
2025-06-05 12:46:40 +0100

    rrrr

--- Sources/RichTextEditor/RichTextEditor.swift
+++ Sources/RichTextEditor/RichTextEditor.swift
@@ -50,7 +50,7 @@ public struct RichTextEditor: NSViewRepresentable {
     public func updateNSView(_ nsView: NSTextView, context: Context) {
         // Only update from outside changes
       
-        if nsView.textStorage?.string != attributedText.string {
+        if nsView.attributedString() != attributedText {
             nsView.textStorage?.setAttributedString(attributedText)
         }
     }
